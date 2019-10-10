fde73ff8e5056ae25dc2ab7d4fde799e5774c319
U-HQ001\warcgr01 <warcgr01@SJVJY1X2.HQ001.CORP.SFY>
2019-10-10 09:01:50 +0200

    pierwszy komit

---
+++ src/c_app.c
@@ -0,0 +1,21 @@
+/*
+ ============================================================================
+ Name        : c_app.c
+ Author      : Grzegorz Warchol
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+	while(1){
+
+	}
+	return EXIT_SUCCESS;
+}
